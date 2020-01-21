38ef3f49ffd4c6f39c5fe23c1e5fd8c320bd2638
SoTosorrow <1064005438@qq.com>
2020-01-21 13:41:59 +0800

    2020-1-20

--- ML library/sklearn_knn.py
+++ ML library/sklearn_knn.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
                                  leaf_size=30,  # 叶子数，默认30
                                  p=2,
                                  metric='minkowski',  # 距离的度量，默认明可夫斯基，p=2时，明可夫斯基距离等于欧氏距离
+                                 n_jobs=1  # 线程个数
                                  )
     # 训练模型
     model.fit(x,y)
@@ -19,3 +20,5 @@ if __name__ == "__main__":
     predict_data = [[2,2]]  # 输入数据是二维的，所以预测数据也要是二维的
     result = model.predict(predict_data)
     print(result)
+    result_probability = model.predict_proba(predict_data)  # 结果分布的概率
+    print(result_probability)
